576e9220d7a3380ed2134c8174254e0012df8120
backnback <garam8796@naver.com>
2024-05-29 17:07:07 +0900

    gradle

---
+++ Main.java
@@ -0,0 +1,20 @@
+// public class Main {
+//     public static void main(String[] args) {
+        
+//         System.out.println("Hello World!");
+
+
+//     }
+// }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a / b);
+
+    }
+}
+
--- java-lang/app/src/main/java/Hello.java
+++ java-lang/app/src/main/java/Hello.java
@@ -1,14 +1,14 @@
 public class Hello {
-    
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
 
         int a = 100;
 
+        // 테스트입니다.
         if (a < 100) {
             System.out.println("하하하");
         }
-
 
     }
 
