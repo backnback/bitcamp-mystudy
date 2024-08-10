eb5a3a53c80dcd90d090e2f5aef9f86c307cb63d
backnback <garam8796@naver.com>
2024-08-10 17:04:59 +0900

    8/10

--- myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/ProjectDaoImpl.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/ProjectDaoImpl.java
@@ -68,12 +68,47 @@ public class ProjectDaoImpl implements ProjectDao {
 
   @Override
   public Project findBy(int no) throws Exception {
-    return null;
+    try (// SQL을 서버에 전달할 객체 준비
+        Statement stmt = con.createStatement();
+
+        // select 문 실행을 요청한다.
+        ResultSet rs = stmt.executeQuery("select * from myapp_projects where project_id=" + no)) {
+
+
+      if (rs.next()) {  // select 실행 결과에서 1 개의 레코드를 가져온다.
+        Project project = new Project();
+        project.setNo(rs.getInt("project_Id"));
+        project.setTitle(rs.getString("title"));
+        project.setDescription(rs.getString("description"));
+        project.setStartDate(rs.getString("start_date"));
+        project.setEndDate(rs.getString("end_date"));
+
+        return project;
+      }
+      return null;
+    }
   }
 
   @Override
   public boolean update(Project project) throws Exception {
-    return false;
+    try (// SQL을 서버에 전달할 객체 준비
+        Statement stmt = con.createStatement();) {
+
+      // update 문 전달
+      int count = stmt.executeUpdate(String.format(
+          "update myapp_projects set"
+              + " title='%s',"
+              + " description='%s',"
+              + " start_date='%s',"
+              + " end_date='%s' where board_id='%d'",
+          project.getTitle(),
+          project.getDescription(),
+          project.getStartDate(),
+          project.getEndDate(),
+          project.getNo()));
+
+      return count > 0;
+    }
   }
 
   @Override
